3830d343fdc2911b0a9d387b475ee9561d1ec379
Aurino Salvador <aurinosalvador@gmail.com>
2021-08-25 16:18:42 -0300

    Implements number verify.

--- bin/desafio01.dart
+++ bin/desafio01.dart
@@ -1,3 +1,31 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Insira um número: ');
+  String input = stdin.readLineSync() ?? '';
+  int num;
+  try {
+    num = int.parse(input);
+
+    if (num.isNegative) throw 'Número não pode ser negativo.';
+
+    print('Soma: ${verifyNumber(num)}');
+  } catch (e) {
+    if (e.runtimeType == FormatException) {
+      print('Valor inválido');
+    } else {
+      print(e);
+    }
+  }
+}
+
+int verifyNumber(int number) {
+  int result = 0;
+
+  for (int i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      result += i;
+    }
+  }
+  return result;
 }
